a77801048962c9a1c740ad4f3e52f7d6d54ac348
Dante <dante.nkaddarian@gmail.com>
2023-01-13 16:04:49 -0300

    cambiado el puerto

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //api entry
 const server = require("./src/app");
 const { conn } = require("./src/db");
+const {PGPORT} = process.env;
 
 // Syncing all the models at once.
-const port = process.env.PGPORT || 3001;
+// const port = process.env.PGPORT || 3001;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+  server.listen(PGPORT, () => {
+    console.log(`%s listening at ${PGPORT}`); // eslint-disable-line no-console
   });
 });
