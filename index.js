45015e4c0728757f7455b3f56f22229fba7d7aa9
Dante <dante.nkaddarian@gmail.com>
2023-01-13 20:34:16 -0300

    prueba deploy 1

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 //api entry
 const server = require("./src/app");
 const { conn } = require("./src/db");
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Syncing all the models at once.
 // const port = process.env.PGPORT || 3001;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
-  });
+  server.listen(PORT);
 });
