cb65bc3032e7ff0a54a3691d5e5beaad801a2cb0
Dante <dante.nkaddarian@gmail.com>
2023-01-16 12:03:03 -0300

    actualizados CORS

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ server.use(cors());
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://tiendanuestra.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://159.223.195.60:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
