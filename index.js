f8ef1751c2cd53f40e51bbfe51aa3a7bc173c0ec
Dante <dante.nkaddarian@gmail.com>
2023-01-17 12:30:39 -0300

    cambiados los datos de la db

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //api entry
 const server = require("./src/app");
 const { conn } = require("./src/db");
-const PORT = process.env.PORT || 3001;
+/* const PORT = process.env.PORT || 3001; */
+
+const port = process.env.port || 3001;
 
 // Syncing all the models at once.
 // const port = process.env.PGPORT || 3001;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
--- src/db.js
+++ src/db.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER , DB_PASSWORD, DB_HOST} = process.env;
+/* const { DB_USER , DB_PASSWORD, DB_HOST} = process.env; */
+const { username , password, host} = process.env; 
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/clothing-ecommerce`,
+  `postgres://${username}:${password}@${host}/defaultdb`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
