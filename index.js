51a9ca7e8a61d8ec34b3377ca4fa64a898170ad8
Dante <dante.nkaddarian@gmail.com>
2023-01-23 16:55:44 -0300

    pruebas de deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //api entry
 const server = require("./src/routes/app");
 const { conn } = require("./src/db");
-const PORT = process.env.PORT || 3001;
+//const PORT = process.env.PORT || 3001;
 
 
 // Syncing all the models at once.
-// const port = process.env.PGPORT || 3001;
+const PORT = process.env.port || 3001;
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
--- src/db.js
+++ src/db.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER , DB_PASSWORD, DB_HOST, database} = process.env;
+const { username , password, host, database} = process.env;
 
+/* postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${database} */
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${database}`,
+  `postgres://${username}:${password}@${host}/${database}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
