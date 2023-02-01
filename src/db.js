5d439842f01290ce03af6eb7245f5dfbf0a4d980
Dante <dante.nkaddarian@gmail.com>
2023-02-01 13:09:55 -0300

    comentados CORS

--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { username , password, host, database} = process.env;
+const { DB_USER , DB_PASSWORD, DB_HOST, DATABASE} = process.env;
+//postgres://${username}:${password}@${host}/${database}?sslmode=require
 
-/* postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${database} */
 const sequelize = new Sequelize(
-  `postgres://${username}:${password}@${host}/${database}?sslmode=require`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
