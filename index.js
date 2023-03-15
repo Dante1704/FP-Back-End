10f3cb03db312c2ba9adc64a3690e5c2006ddf6a
Dante <dante.nkaddarian@gmail.com>
2023-03-15 15:20:59 -0300

    preparando deploy en vercel

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const { conn } = require("./src/db");
 
 
 // Syncing all the models at once.
-//const PORT = process.env.port || 3001;
+const APP_PORT = process.env.port || 3001;
 
-conn.sync({ force: true }).then(() => {
+/* conn.sync({ force: true }).then(() => {
   server.listen(8080);
+});
+ */
+
+conn.sync({}).then(() => {
+  server.listen(APP_PORT, () => {
+    console.log(`%s listening at ${APP_PORT}`); // eslint-disable-line no-console
+  });
 });
